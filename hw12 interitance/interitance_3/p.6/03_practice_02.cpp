24b74370182daa7ad1e09546119470a700cae4de
for122rest <146347513+for122rest@users.noreply.github.com>
2023-11-24 15:22:02 +0900

    Add files via upload

---
+++ hw12 interitance/interitance_3/p.6/03_practice_02.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+	A() : ii(10) {};
+	A(int x) : ii(x) {};
+	int ii;
+};
+
+class B : public A {
+public:
+	B() : A(8) { A(88); bbb = ii + 5; ccc = ii * 10; };
+	B(int x) : A() { bbb = x; ccc = ii * 10; };
+	int bbb, ccc;
+};
+
+void main() {
+	B e, f(22);
+	cout << " e.bbb = " << e.bbb << endl;
+	cout << " f.bbb = " << f.bbb << endl;
+	cout << " e.ccc = " << e.ccc << endl;
+	cout << " f.ccc = " << f.ccc << endl;
+}
